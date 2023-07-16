2778e7920103023a703e1bf30dafc9ac9e49a250
youyou <705592307@qq.com>
2023-07-16 23:57:57 +0800

    add kitty

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -7,5 +7,8 @@ alias info='neofetch'
 alias fm='joshuto'
 alias y='yadm'
 
+alias icat="kitty +kitten icat"
+alias diff="kitty +kitten diff"
+
 
 alias fpv='fzf --preview "bat --style=numbers --color=always --line-range :500 {}"'
