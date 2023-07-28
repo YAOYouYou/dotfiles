1a3d53fa6a87419e37c4e95e7262b370e48ad32a
youyou <705592307@qq.com>
2023-07-29 04:13:36 +0800

    update

--- .config/nvim/lua/plugins/lazyvim.lua
+++ .config/nvim/lua/plugins/lazyvim.lua
@@ -1,20 +1,4 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    -- enabled = false,
-    config = function()
-      require("neo-tree").setup({
-        filesystem = {
-          window = {
-            mappings = {
-              ["o"] = "open",
-            },
-          },
-        },
-      })
-    end,
-  },
-
   { "navarasu/onedark.nvim" },
 
   {
