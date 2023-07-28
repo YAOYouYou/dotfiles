630a2dc19d136bda96f69e8f1e304ed6a2925692
youyou <705592307@qq.com>
2023-07-29 03:25:30 +0800

    update

--- .config/nvim/lua/plugins/lazyvim.lua
+++ .config/nvim/lua/plugins/lazyvim.lua
@@ -25,4 +25,22 @@ return {
   },
   { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin/nvim", name = "catppuccin", enabled = false },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {})
+      end
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {})
+      end
+    end,
+  },
 }
