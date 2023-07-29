350268272dff1494df38c62606c0c8a3dee205b5
youyou <705592307@qq.com>
2023-07-30 01:14:00 +0800

    upte

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-require("lazyvim.plugins.extras.lang.rust")
-require("lazyvim.plugins.extras.")
-require("lazyvim.plugins.extras.vscode")
